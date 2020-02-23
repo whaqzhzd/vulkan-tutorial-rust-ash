b3dfa1f1648bbdb9f2238cac9d488cc39e1bdc8f
whaqzhzd <544430497@qq.com>
2020-02-23 15:27:08 +0800

    提交纹理绑定完整实现 修改readme添加运行成功快照

--- src/shader/compile.bat
+++ src/shader/compile.bat
@@ -6,3 +6,6 @@ glslc vertex_input_shader.frag -o vertex_input_frag.spv
 
 glslc ubo_shader.vert -o ubo_shader_vert.spv
 glslc ubo_shader.frag -o ubo_shader_frag.spv
+
+glslc image_sampler_shader.vert -o image_sampler_vert.spv
+glslc image_sampler_shader.frag -o image_sampler_frag.spv
